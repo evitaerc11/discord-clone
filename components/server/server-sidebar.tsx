cc1629669a71a021c79eab9cb3f31f53e84a065f
NAM! <abcdxyz14144@gmail.com>
2023-12-23 10:02:23 +0700

    List & edit & delete channel

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -9,9 +9,9 @@ import { db } from "@/lib/db";
 
 import { ServerHeader } from "./server-header";
 import { ServerSearch } from "./server-search";
-// import { ServerSection } from "./server-section";
-// import { ServerChannel } from "./server-channel";
-// import { ServerMember } from "./server-member";
+import { ServerSection } from "./server-section";
+import { ServerChannel } from "./server-channel";
+import { ServerMember } from "./server-member";
 
 interface ServerSidebarProps {
   serverId: string;
@@ -122,80 +122,80 @@ export const ServerSidebar = async ({
         <Separator className="bg-zinc-200 dark:bg-zinc-700 rounded-md my-2" />
         {!!textChannels?.length && (
           <div className="mb-2">
-            {/* <ServerSection
+            <ServerSection
               sectionType="channels"
               channelType={ChannelType.TEXT}
               role={role}
               label="Text Channels"
-            /> */}
+            />
             <div className="space-y-[2px]">
-              {/* {textChannels.map((channel) => (
+              {textChannels.map((channel) => (
                 <ServerChannel
                   key={channel.id}
                   channel={channel}
                   role={role}
                   server={server}
                 />
-              ))} */}
+              ))}
             </div>
           </div>
         )}
         {!!audioChannels?.length && (
           <div className="mb-2">
-            {/* <ServerSection
+            <ServerSection
               sectionType="channels"
               channelType={ChannelType.AUDIO}
               role={role}
               label="Voice Channels"
-            /> */}
+            />
             <div className="space-y-[2px]">
-              {/* {audioChannels.map((channel) => (
+              {audioChannels.map((channel) => (
                 <ServerChannel
                   key={channel.id}
                   channel={channel}
                   role={role}
                   server={server}
                 />
-              ))} */}
+              ))}
             </div>
           </div>
         )}
         {!!videoChannels?.length && (
           <div className="mb-2">
-            {/* <ServerSection
+            <ServerSection
               sectionType="channels"
               channelType={ChannelType.VIDEO}
               role={role}
               label="Video Channels"
-            /> */}
+            />
             <div className="space-y-[2px]">
-              {/* {videoChannels.map((channel) => (
+              {videoChannels.map((channel) => (
                 <ServerChannel
                   key={channel.id}
                   channel={channel}
                   role={role}
                   server={server}
                 />
-              ))} */}
+              ))}
             </div>
           </div>
         )}
         {!!members?.length && (
           <div className="mb-2">
-            {/* <ServerSection
+            <ServerSection
               sectionType="members"
               role={role}
               label="Members"
               server={server}
-            /> */}
+            />
             <div className="space-y-[2px]">
-              {/* {members.map((member) => (
+              {members.map((member) => (
                 <ServerMember
                   key={member.id}
                   member={member}
                   server={server}
                 />
-              ))} */}
+              ))}
             </div>
           </div>
         )}
