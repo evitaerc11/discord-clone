4882d90c8410f350364cf1842819ef4b36ec7b86
NAM! <abcdxyz14144@gmail.com>
2023-12-23 15:05:27 +0700

    Chat header & setup socket

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,8 +1,81 @@
-import React from 'react'
+import { currentProfile } from '@/lib/current-profile'
+import { redirectToSignIn } from '@clerk/nextjs'
+import { db } from '@/lib/db'
+import { redirect } from 'next/navigation'
+import { ChatHeader } from '@/components/chat/chat-header'
 
-const ChannelIdPage = () => {
+interface ChannelIdPageProps {
+  params: {
+    serverId: string
+    channelId: string
+  }
+}
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile()
+
+  if (!profile) {
+    return redirectToSignIn()
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId
+    }
+  })
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id
+    }
+  })
+
+  if (!member || !channel) {
+    redirect('/')
+  }
+
   return (
-    <div>ChannelIdPage</div>
+    <div className='bg-white dark:bg-[#313338] flex flex-col h-full'>
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type='channel'
+      />
+      {/* {channel.type === ChannelType.TEXT && (
+        <>
+          <ChatMessages
+            member={member}
+            name={channel.name}
+            chatId={channel.id}
+            type='channel'
+            apiUrl='/api/messages'
+            socketUrl='/api/socket/messages'
+            socketQuery={{
+              channelId: channel.id,
+              serverId: channel.serverId
+            }}
+            paramKey='channelId'
+            paramValue={channel.id}
+          />
+          <ChatInput
+            name={channel.name}
+            type='channel'
+            apiUrl='/api/socket/messages'
+            query={{
+              channelId: channel.id,
+              serverId: channel.serverId
+            }}
+          />
+        </>
+      )} */}
+      {/* {channel.type === ChannelType.AUDIO && (
+        <MediaRoom chatId={channel.id} video={false} audio={true} />
+      )} */}
+      {/* {channel.type === ChannelType.VIDEO && (
+        <MediaRoom chatId={channel.id} video={true} audio={true} />
+      )} */}
+    </div>
   )
 }
 
